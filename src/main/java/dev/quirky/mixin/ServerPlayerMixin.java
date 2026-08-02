25777485ebf6a0f8c8695a86286c32737369b71e
mexiaosqwq <1078628894@qq.com>
2026-08-02 10:18:53 +0800

    feat: notify player of totem location, dimension and distance on death

--- src/main/java/dev/quirky/mixin/ServerPlayerMixin.java
+++ src/main/java/dev/quirky/mixin/ServerPlayerMixin.java
@@ -4,11 +4,14 @@ import dev.quirky.ModEntities;
 import dev.quirky.totem.TotemEntity;
 import dev.quirky.totem.TotemOfHoldingLogic;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.ItemStackWithSlot;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gamerules.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -47,6 +50,29 @@ public class ServerPlayerMixin {
 		totem.initStored(player.getUUID(), stored);
 		player.level().addFreshEntity(totem);
 		player.level().playSound(null, totem.blockPosition(), SoundEvents.AMETHYST_BLOCK_CHIME, SoundSource.BLOCKS, 1.0F, 1.0F);
+		BlockPos spawnPos;
+		ServerPlayer.RespawnConfig respawnConfig = player.getRespawnConfig();
+		if (respawnConfig != null) {
+			spawnPos = respawnConfig.respawnData().globalPos().pos();
+		} else {
+			spawnPos = player.level().getLevelData().getRespawnData().globalPos().pos();
+		}
+		ResourceKey<Level> deathDimension = player.level().dimension();
+		String dimensionKey = "dimension." + deathDimension.identifier().getNamespace() + "." + deathDimension.identifier().getPath();
+		Component dimensionName = Component.translatableWithFallback(dimensionKey, dimensionKey);
+		boolean sameDimension = deathDimension.equals(player.getRespawnConfig() != null
+			? player.getRespawnConfig().respawnData().globalPos().dimension()
+			: player.level().getLevelData().getRespawnData().globalPos().dimension());
+		if (sameDimension) {
+			int dx = pos.getX() - spawnPos.getX();
+			int dz = pos.getZ() - spawnPos.getZ();
+			int distance = (int) Math.round(Math.sqrt(dx * dx + dz * dz));
+			player.sendSystemMessage(Component.translatable("message.quirky.totem_spawned",
+				dimensionName, pos.getX(), pos.getY(), pos.getZ(), distance));
+		} else {
+			player.sendSystemMessage(Component.translatable("message.quirky.totem_spawned_cross",
+				dimensionName, pos.getX(), pos.getY(), pos.getZ()));
+		}
 		player.getInventory().clearContent();
 	}
 }
