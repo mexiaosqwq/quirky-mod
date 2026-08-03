543b7638b4b71a0ee64d4673983b2b29c912e719
mexiaosqwq <1078628894@qq.com>
2026-08-03 13:26:23 +0800

    chore: drop internal review tag from MetalButtonBlock comment

--- src/main/java/dev/quirky/block/MetalButtonBlock.java
+++ src/main/java/dev/quirky/block/MetalButtonBlock.java
@@ -42,7 +42,7 @@ public class MetalButtonBlock extends ButtonBlock {
 
 	public MetalButtonBlock(final BlockSetType type, final int holdTicks, final BlockBehaviour.Properties properties) {
 		super(type, holdTicks, properties);
-		// 父类 ButtonBlock 的 ticksToStayPressed 是 private，镜像存储一份供测试可读性（review S6）
+		// 父类 ButtonBlock 的 ticksToStayPressed 是 private，镜像存储一份供测试可读性
 		this.holdTicks = holdTicks;
 	}
 
